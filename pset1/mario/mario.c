cedf2325376aa408f138613d74c51a4c72284eec
AlphaBravo7777 <yasargon@gmail.com>
2018-08-26 23:05:29 +0300

    Upload files for pset1

---
+++ pset1/mario/mario.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+    int h = 0, height = 0;
+
+    do {
+        height = get_int("height: ");
+        if (height == 0)
+            return 0;
+    } while ((height < 1) || (height > 23));
+
+    do {
+        for (int i = 0; i < height - (h+1); i++)
+            printf(" ");
+        for (int i = 0; i < h; i++)
+            printf("#");
+        printf("##\n");
+        h++;
+    } while (h != height);
+}
