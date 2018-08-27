f014559b680ec0b56276d18e875715570bf2db81
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2018-08-27 20:23:05 +0300

    Update greedy_V2.c

--- pset1/greedy/greedy_V2.c
+++ pset1/greedy/greedy_V2.c
@@ -7,17 +7,15 @@ int main(void) {
         change =  get_float("How much change is owed?: ") * 10000/100;
     } while (change < 0);
 
-    int count = 0;
-
-    count += change / 25;
+    int count = change / 25;
+    
     change %= 25;
-
     count += change / 10;
+    
     change %= 10;
-
     count += change / 5;
+    
     change %= 5;
-
     count += change;
 
     printf("%i\n", count);
