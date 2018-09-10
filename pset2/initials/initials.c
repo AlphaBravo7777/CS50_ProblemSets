0e3f28adc308cdc678984c87314cb4c43b5fcddc
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2018-09-10 17:32:55 +0300

    Update initials.c

--- pset2/initials/initials.c
+++ pset2/initials/initials.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
-void printInitial(char key) {
+void printInitial(char key)
+{
     if (key == 32)
         return;
     if ((key > 96) && (key < 123))
@@ -10,12 +11,16 @@ void printInitial(char key) {
     printf("%c", key);
 }
 
-int main() {
+int main()
+{
     string names = get_string("What is your names?:\n");
+    
     printInitial(names[0]);
+    
     for (int i = 0, n = strlen(names); i < n; i++) {
         if (names[i-1] == 32)
             printInitial(names[i]);
     }
+    
     printf("\n");
 }
