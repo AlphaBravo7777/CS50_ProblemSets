08a902bd998b4f32e28d5276fa8612cda43024ac
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2018-10-03 15:21:25 +0300

    Update metadatabmp.c

--- pset4/bmp/metadatabmp.c
+++ pset4/bmp/metadatabmp.c
@@ -15,28 +15,25 @@
 int main(int argc, char* argv[])
 {
     // ensure proper usage
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("Usage: ./copy infile\n");
         return 1;
     }
 
     // remember filenames
-    char* infile = argv[1];
-    char* outfile = "metadatabmp.txt";
+    char *infile = argv[1];
+    char *outfile = "metadatabmp.txt";
 
     // open input file
-    FILE* inptr = fopen(infile, "r");
-    if (inptr == NULL)
-    {
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL) {
         printf("Could not open %s.\n", infile);
         return 2;
     }
 
     // open output file
-    FILE* outptr = fopen(outfile, "w");
-    if (outptr == NULL)
-    {
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL) {
         fclose(inptr);
         fprintf(stderr, "Could not create %s.\n", outfile);
         return 3;
@@ -52,8 +49,7 @@ int main(int argc, char* argv[])
 
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
-        bi.biBitCount != 24 || bi.biCompression != 0)
-    {
+    bi.biBitCount != 24 || bi.biCompression != 0) {
         fclose(outptr);
         fclose(inptr);
         fprintf(stderr, "Unsupported file format.\n");
