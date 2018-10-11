942167334859ad43de102fe3d3c2788a9629f42e
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2018-10-11 15:32:21 +0300

    Update dictionary.h

--- pset5/dictionary.h
+++ pset5/dictionary.h
@@ -31,14 +31,14 @@ typedef struct dictTrie {
 } dictTrie;
 
 /**
- * Returns true if word is in dictionary else false.
- */
-bool check(const char *word);
-
-/**
  * Loads dictionary into memory.  Returns true if successful else false.
  */
 bool load(const char *dictionary);
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char *word);
 
 /**
  * Returns number of words in dictionary if loaded else 0 if not yet loaded.
