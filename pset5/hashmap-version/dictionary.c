ac923cd27cc27078685aae68ce651825164617f0
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2018-10-20 21:21:07 +0300

    Update dictionary.c

--- pset5/hashmap-version/dictionary.c
+++ pset5/hashmap-version/dictionary.c
@@ -1,3 +1,12 @@
+/**
+ * dictionary.c
+ *
+ * Computer Science 50
+ * Problem Set 5
+ *
+ * Implements a dictionary's functionality.
+ */
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
