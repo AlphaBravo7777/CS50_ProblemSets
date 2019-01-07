2c77808f37ec5c9a409098d80c05f44ee1aafd56
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2019-01-07 20:43:25 +0300

    Update caesar.c

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -2,26 +2,25 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, string argv[])
 {
     if (argc != 2) {
-        printf("Usage: ./caesar k\n");
+        printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    for (int i = 0, argvLength = strlen(argv[1]); i < argvLength; i++) {
+    for (int i = 0; argv[1][i]; i++) {
         if (!isdigit(argv[1][i])) {
-            printf("The key must be an unsigned integer type\n");
+            printf("Key must be a non-negative integer.\n");
             return 1;
         }
     }
 
     int key = atoi(argv[1]);
-    string text = get_string("Enter the text for encryption with the Caesar cipher:\n");
+    string text = get_string("plaintext: ");
 
-    for (int i = 0, textLength = strlen(text); i < textLength; i++) {
+    for (int i = 0; text[i]; i++) {
         if (isupper(text[i]))
             text[i] = (text[i] - 'A' + key) % 26 + 'A';
         if (islower(text[i]))
