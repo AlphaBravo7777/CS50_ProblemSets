b34daf4e2da6bfd6191f880afcf1f67ff19c28cd
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2019-01-07 15:11:49 +0300

    Update initials.c

--- pset2/initials/initials.c
+++ pset2/initials/initials.c
@@ -1,18 +1,15 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-    string names = get_string("What is your names?:\n");
+    string names = get_string();
 
-    if (!isblank(names[0]))
-        printf("%c", toupper(names[0]));
-
-    for (int i = 0, n = strlen(names); i < n; i++) {
-        if (isblank(names[i-1]) && !isblank(names[i]))
-            printf("%c", toupper(names[i]));
+    printf("%c", toupper(names[0]));
+    for (int i = 0; names[i]; i++) {
+        if (isblank(names[i]))
+            printf("%c", toupper(names[i+1]));
     }
 
     printf("\n");
