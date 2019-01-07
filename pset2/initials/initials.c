dc3930a0ab4cd6bfc9cd2224f98d253238de7d70
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2019-01-07 15:12:50 +0300

    Update initials.c

--- pset2/initials/initials.c
+++ pset2/initials/initials.c
@@ -5,8 +5,8 @@
 int main()
 {
     string names = get_string();
-
     printf("%c", toupper(names[0]));
+    
     for (int i = 0; names[i]; i++) {
         if (isblank(names[i]))
             printf("%c", toupper(names[i+1]));
