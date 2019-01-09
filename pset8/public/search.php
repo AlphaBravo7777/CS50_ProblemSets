a5e5989cd1d900681adbbfceb40a8c031207d7fc
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2019-01-09 18:10:04 +0300

    Update search.php

--- pset8/public/search.php
+++ pset8/public/search.php
@@ -5,7 +5,7 @@
     foreach ($substrings as $i => $substring)
         $substrings[$i] = "+(".$substring."*)";
 
-    $_GET["geo"] = implode (" ", $substrings);
+    $_GET["geo"] = implode(" ", $substrings);
     $places = CS50::query("SELECT * FROM places WHERE MATCH(postal_code, place_name, admin_name1) AGAINST (? IN BOOLEAN MODE)", $_GET["geo"]);
 
     header("Content-type: application/json");
