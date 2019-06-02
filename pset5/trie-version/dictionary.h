6a017d9c993d44656fe6cc3eb99151772cab1176
Sargon <42096865+AlphaBravo7777@users.noreply.github.com>
2019-06-02 13:58:08 +0300

    Update dictionary.h

--- pset5/trie-version/dictionary.h
+++ pset5/trie-version/dictionary.h
@@ -17,14 +17,14 @@
 #define LENGTH 45
 
 /**
- * Returns true if word is in dictionary else false.
- */
-bool check(const char *word);
-
-/**
  * Loads dictionary into memory.  Returns true if successful else false.
  */
 bool load(const char *dictionary);
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char *word);
 
 /**
  * Returns number of words in dictionary if loaded else 0 if not yet loaded.
